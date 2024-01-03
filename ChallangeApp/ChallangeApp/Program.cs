013444c3d3e79e64f110ad78daff2655cdbbaad0
AdamEys <eysymont@yahoo.com>
2024-01-03 22:58:55 +0100

    Zadanie domowe D4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// ==  równa się
+// != nie równa się
+// >= mniejsze równe
+// <= większe równe
+// && warunek "i"
+// || warunek "lub"
+// ! negacja
+
+var age = 15;
+var name = "Ewa";
+string sex = "mężczyzna";
+
+if (age < 30 && sex == "kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 30 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (age < 18 && sex == "mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+
+}
